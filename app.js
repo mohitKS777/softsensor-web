3112ff34fb09862330aece5fc2e894d7c1e79cd0
namandangi <danginaman55@gmail.com>
2021-04-23 21:13:17 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const cors = require('cors');
+const path = require('path')
+const {spawn} = require('child_process')
+
+
+app.use(cors());
+
+let guestList = []
+
+io.on("connection", (socket) => {
+  console.log("Socket ID: "+socket.id)
+
+  socket.on("join_room", (data) => {
+    guestList.push(data.guest)
+    socket.join(data.room);
+    console.log("A User Joined Room: ", data.room);
+    console.log('Guest List', guestList);
+  });
+
+  socket.on("leave_room", (data) => {
+    socket.leave(data.roomName);
+    guestList = guestList.filter(guest => guest.username !== data.username);
+    console.log("User: ",data.username, "leaved Room: " + data.roomName);
+    console.log('Guest List', guestList);
+  });
+  
+  socket.on("count_guests", (data) => {
+    let room = io.sockets.adapter.rooms.get(data);
+    if(room){
+      let guestCount = room.size;
+      socket.to(data).emit("get_count", guestCount);
+    }
+  });
+
+  socket.on("send_guest_list", (data) => {
+    console.log("received guest list request for room: ", data.roomName);
+    console.log("sent to: ",data.alias,"guest list ", guestList);
+    socket.to(data.roomName).emit("receive_guest_list", guestList);
+  })
+
+  socket.on("send_annotations", (data) => {
+    data = JSON.parse(data);
+    const { username, content } = data;
+    console.log("annotations received: ",data);
+    socket.to(data.roomName).emit("receive_annotations", { username, content });
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data.content);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+
+http.listen(8080,()=> {
+    console.log('listening on port 8080');
+})
