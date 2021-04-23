455871d8cb5c9c79e8a05391de26c7b6e11ee3c7
namandangi <danginaman55@gmail.com>
2021-04-23 22:33:40 +0530

    add Text tool

---
+++ webapp/src/components/Text/fontPicker.js
@@ -0,0 +1,102 @@
+import React from "react";
+// import PropTypes from 'prop-types';
+import { useFabricOverlayState } from "../../context/fabric-overlay-context";
+import {
+  Link,
+  Heading,
+  VStack,
+  Wrap,
+  WrapItem,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import FontFaceObserver from "fontfaceobserver";
+
+export const fonts = [
+  {
+    id: "reenieBeanie",
+    fontFamily: "Reenie Beanie",
+  },
+  {
+    id: "courierPrime",
+    fontFamily: "Courier Prime",
+  },
+  {
+    id: "openSans",
+    fontFamily: "Open Sans",
+  },
+];
+
+function TypeTextFontPicker({ activeFont, handleFontChange }) {
+  const { fabricOverlay } = useFabricOverlayState();
+
+  const activeClasses = {
+    borderColor: useColorModeValue("gray.300", "white"),
+    borderWidth: "2px",
+  };
+
+  const handleFontClick = (font) => {
+    handleFontChange(font);
+  };
+
+  const loadAndUse = (font) => {
+    const canvas = fabricOverlay.fabricCanvas();
+    const activeObject = canvas.getActiveObject();
+
+    if (!activeObject) {
+      return;
+    }
+
+    var myfont = new FontFaceObserver(font);
+    myfont
+      .load()
+      .then(function () {
+        // when font is loaded, use it.
+        canvas.getActiveObject().set("fontFamily", font);
+        canvas.requestRenderAll();
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  };
+
+  return (
+    <>
+      <Heading as="h2" size="sm" mb={3}>
+        Text
+      </Heading>
+      <VStack spacing={3}>
+        <Wrap direction="column" justify="flex-start">
+          {fonts.map((font) => (
+            <WrapItem key={font.id}>
+              <Link
+                fontFamily={font.fontFamily}
+                id={font.id}
+                onClick={() => handleFontClick(font)}
+                fontSize="24px"
+                w="250px"
+                whiteSpace="nowrap"
+                overflow="hidden"
+                textOverflow="ellipsis"
+                borderWidth="1px"
+                p={3}
+                borderRadius={8}
+                {...(activeFont &&
+                  activeFont.id === font.id && { ...activeClasses })}
+              >
+                {font.fontFamily}
+              </Link>
+            </WrapItem>
+          ))}
+        </Wrap>
+      </VStack>
+    </>
+  );
+}
+
+// TypeTextFontPicker.propTypes = {
+//   activeFont: PropTypes.object,
+//   handleFontChange: PropTypes.func,
+//   handlePreviewTextChange: PropTypes.func,
+// };
+
+export default TypeTextFontPicker;
---
+++ webapp/src/components/Text/text.js
@@ -0,0 +1,203 @@
+import React from "react";
+// import PropTypes from 'prop-types';
+import ToolbarButton from "../Toolbar/button";
+import { FiType } from "react-icons/fi";
+import { fabric } from "openseadragon-fabricjs-overlay";
+import {
+  useFabricOverlayDispatch,
+  useFabricOverlayState,
+} from "../../context/fabric-overlay-context";
+import TypeTextFontPicker from "./fontPicker";
+import ToolbarOptionsPanel from "../Toolbar/optionsPanel";
+import { fonts } from "./fontPicker";
+import FontFaceObserver from "fontfaceobserver";
+import useFabricHelpers from "../../hooks/use-fabric-helpers";
+
+function TypeText({ isActive }) {
+  const dispatch = useFabricOverlayDispatch();
+  const { color, fabricOverlay, viewer } = useFabricOverlayState();
+  const { deselectAll } = useFabricHelpers();
+
+  const [myState, _setMyState] = React.useState({
+    activeFont: fonts[0],
+    color,
+    isActive, // Is the main Type tool active
+    isEditing: false,
+    // TODO: Remove selectedCoords?
+    selectedCoords: { top: 0, left: 0 },
+  });
+  const myStateRef = React.useRef(myState);
+  const setMyState = (data) => {
+    myStateRef.current = data;
+    _setMyState(data);
+  };
+
+  /**
+   * Handle main tool change
+   */
+  React.useEffect(() => {
+    setMyState({ ...myState, color, isActive });
+
+    if (!fabricOverlay) return;
+    fabricOverlay.fabricCanvas().defaultCursor = isActive ? "text" : "auto";
+  }, [color, isActive]);
+
+  React.useEffect(() => {
+    if (!isActive) return;
+    if (myState.isEditing) {
+      fabricOverlay.fabricCanvas().defaultCursor = "auto";
+      fabricOverlay.fabricCanvas().hoverCursor = "text";
+    } else {
+      fabricOverlay.fabricCanvas().defaultCursor = "text";
+      fabricOverlay.fabricCanvas().hoverCursor = "move";
+    }
+  }, [myState.isEditing]);
+
+  /**
+   * Handle an individual font being selected
+   */
+  React.useEffect(() => {
+    if (!fabricOverlay) return;
+    const canvas = fabricOverlay.fabricCanvas();
+
+    if (myState.activeFont) {
+      // Disable OSD mouseclicks
+      viewer.setMouseNavEnabled(false);
+      viewer.outerTracker.setTracking(false);
+
+      // Deselect all Fabric Canvas objects
+      deselectAll();
+    } else {
+      // Enable OSD mouseclicks
+      viewer.setMouseNavEnabled(true);
+      viewer.outerTracker.setTracking(true);
+    }
+  }, [myState.activeFont]);
+
+  /**
+   * Set up event handlers when Fabric is ready
+   */
+  React.useEffect(() => {
+    if (!fabricOverlay) return;
+    const canvas = fabricOverlay.fabricCanvas();
+
+    function handleMouseDown(options) {
+      // Selected an existing object OR not in Type Tool mode
+      if (options.target || !myStateRef.current.isActive) {
+        return;
+      }
+
+      // Was user previously editing text?
+      if (myStateRef.current.isEditing) {
+        deselectAll();
+        setMyState({ ...myStateRef.current, isEditing: false });
+        return;
+      }
+
+      // Create new Textbox instance and add it to canvas
+      const textbox = new fabric.IText("", {
+        left: options.absolutePointer.x,
+        top: options.absolutePointer.y,
+        fontFamily: myStateRef.current.activeFont.fontFamily,
+        fontSize: 100,
+        selectionBackgroundColor: "rgba(255, 255, 255, 0.5)",
+      });
+      fabricOverlay.fabricCanvas().add(textbox);
+      textbox.set({ fill: myStateRef.current.color.hex });
+      canvas.setActiveObject(textbox);
+      textbox.enterEditing();
+
+      setMyState({
+        ...myStateRef.current,
+        isEditing: true,
+      });
+    }
+
+    function handleSelectionCleared(options) {
+      if (!myStateRef.current.isSelectedOnCanvas) return;
+
+      setMyState({
+        ...myStateRef.current,
+        selectedCoords: { top: 0, left: 0 },
+      });
+    }
+
+    function handleSelected(options) {
+      if (options.target.get("type") !== "textbox") return;
+
+      setMyState({
+        ...myStateRef.current,
+      });
+    }
+
+    // Add click handlers
+    canvas.on("mouse:down", handleMouseDown);
+    canvas.on("selection:created", handleSelected);
+    canvas.on("selection:updated", handleSelected);
+    canvas.on("selection:cleared", handleSelectionCleared);
+
+    // Remove handler
+    return function clearFabricEventHandlers() {
+      canvas.off("mouse:down", handleMouseDown);
+      canvas.off("selection:created", handleSelected);
+      canvas.off("selection:updated", handleSelected);
+      canvas.off("selection:cleared", handleSelectionCleared);
+    };
+  }, [fabricOverlay]);
+
+  const handleFontChange = (font) => {
+    setMyState({ ...myState, activeFont: font });
+    //loadAndUse(font.fontFamily);
+  };
+
+  const handleToolbarButtonClick = (e) => {
+    dispatch({ type: "updateTool", tool: isActive ? "" : "TYPE" });
+  };
+
+  const loadAndUse = (font) => {
+    const canvas = fabricOverlay.fabricCanvas();
+    const activeObject = canvas.getActiveObject();
+
+    if (!activeObject) {
+      return;
+    }
+
+    var myfont = new FontFaceObserver(font);
+    myfont
+      .load()
+      .then(function () {
+        // when font is loaded, use it.
+        canvas.getActiveObject().set("fontFamily", font);
+        canvas.requestRenderAll();
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  };
+
+  return (
+    <div>
+      <ToolbarButton
+        onClick={handleToolbarButtonClick}
+        icon={<FiType />}
+        isActive={isActive}
+        label="Type Text"
+        color="#fff"
+      />
+      {isActive && (
+        <ToolbarOptionsPanel>
+          <TypeTextFontPicker
+            activeFont={myState.activeFont}
+            handleFontChange={handleFontChange}
+          />
+        </ToolbarOptionsPanel>
+      )}
+    </div>
+  );
+}
+
+// TypeText.propTypes = {
+//   isActive: PropTypes.bool,
+// };
+
+export default TypeText;
