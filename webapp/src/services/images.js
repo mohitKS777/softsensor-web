132bc34d023b4cc9a177053b717765fa45cfab7e
namandangi <danginaman55@gmail.com>
2021-04-23 21:40:22 +0530

    add CMU slides resources for OSD tilesources

---
+++ webapp/src/services/images.js
@@ -0,0 +1,419 @@
+import iceCreamSoda from "../images/ice-cream-soda.jpg";
+
+export const Images = [
+  // {
+  //     id: 0,
+  //     type: 'image',
+  //     url: iceCreamSoda,
+  // },
+  // {
+  //     id: 1,
+  //     type: "image",
+  //     url: "https://openseadragon.github.io/example-images/grand-canyon-landscape-overlooking.jpg"
+  // },
+  {
+    id: 2,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "32893",
+        Width: "46000",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/aperio/cmu-1-jp2k-33005/slide_files/",
+    },
+  },
+  {
+    id: 3,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "2967",
+        Width: "2220",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/aperio/cmu-1-small-region/slide_files/",
+    },
+  },
+  {
+    id: 4,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "32914",
+        Width: "46000",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/aperio/cmu-1/slide_files/",
+    },
+  },
+  {
+    id: 5,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "30462",
+        Width: "78000",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/aperio/cmu-2/slide_files/",
+    },
+  },
+  {
+    id: 6,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "45402",
+        Width: "66000",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/aperio/cmu-3/slide_files/",
+    },
+  },
+  {
+    id: 7,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "17497",
+        Width: "15374",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/aperio/jp2k-33003-1/slide_files/",
+    },
+  },
+  {
+    id: 8,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "47067",
+        Width: "32671",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/aperio/jp2k-33003-2/slide_files/",
+    },
+  },
+  {
+    id: 9,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "32914",
+        Width: "46000",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/generic-tiff/cmu-1/slide_files/",
+    },
+  },
+  {
+    id: 10,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "76288",
+        Width: "102400",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu-vms/cmu-1/slide_files/",
+    },
+  },
+  {
+    id: 11,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "68352",
+        Width: "159744",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu-vms/cmu-2/slide_files/",
+    },
+  },
+  {
+    id: 12,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "68352",
+        Width: "159744",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu-vms/cmu-2/slide_files/",
+    },
+  },
+  {
+    id: 13,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "101888",
+        Width: "143360",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu-vms/cmu-3/slide_files/",
+    },
+  },
+  {
+    id: 14,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "101888",
+        Width: "143360",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu-vms/cmu-3/slide_files/",
+    },
+  },
+  {
+    id: 15,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "38144",
+        Width: "51200",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu/cmu-1/slide_files/",
+    },
+  },
+  {
+    id: 16,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "33792",
+        Width: "79872",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu/cmu-2/slide_files/",
+    },
+  },
+  {
+    id: 17,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "50688",
+        Width: "71680",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu/cmu-3/slide_files/",
+    },
+  },
+  {
+    id: 18,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "110592",
+        Width: "122880",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu/os-1/slide_files/",
+    },
+  },
+  {
+    id: 19,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "73728",
+        Width: "126976",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu/os-2/slide_files/",
+    },
+  },
+  {
+    id: 20,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "77824",
+        Width: "122880",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/hamamatsu/os-3/slide_files/",
+    },
+  },
+  {
+    id: 21,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "38432",
+        Width: "36832",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/leica/leica-1/slide_files/",
+    },
+  },
+  {
+    id: 22,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "139227",
+        Width: "44957",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/leica/leica-2/slide_files/",
+    },
+  },
+  {
+    id: 23,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "27712",
+        Width: "40000",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/trestle/cmu-1/slide_files/",
+    },
+  },
+  {
+    id: 24,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "25408",
+        Width: "61504",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/trestle/cmu-2/slide_files/",
+    },
+  },
+  {
+    id: 25,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "25408",
+        Width: "61504",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/trestle/cmu-2/slide_files/",
+    },
+  },
+  {
+    id: 26,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "38080",
+        Width: "56896",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/trestle/cmu-3/slide_files/",
+    },
+  },
+  {
+    id: 27,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "93951",
+        Width: "105813",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/ventana/os-1/slide_files/",
+    },
+  },
+  {
+    id: 28,
+    Image: {
+      Format: "jpeg",
+      Overlap: "1",
+      TileSize: "510",
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Size: {
+        Height: "76349",
+        Width: "114943",
+      },
+      Url:
+        "https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/ventana/os-2/slide_files/",
+    },
+  },
+];
