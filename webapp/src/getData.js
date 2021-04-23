045de43d5bd84e60b580f9765faa110b2a99445b
namandangi <danginaman55@gmail.com>
2021-04-23 21:46:01 +0530

    add script for scraping gdc portal slides and add store slide ids

---
+++ webapp/src/getData.js
@@ -0,0 +1,16 @@
+// import axios from 'axios';
+
+// async function getdata() {
+//     let data = [];
+//     // let result = await axios.get('https://api.gdc.cancer.gov/files?filters=%7b%0d%0a++%22op%22%3a+%22and%22%2c%0d%0a++%22content%22%3a+%5b%0d%0a++++%7b%0d%0a++++++%22op%22%3a+%22in%22%2c%0d%0a++++++%22content%22%3a+%7b%0d%0a++++++++%22field%22%3a+%22files.data_format%22%2c%0d%0a++++++++%22value%22%3a+%5b%0d%0a++++++++++%22svs%22%0d%0a++++++++%5d%0d%0a++++++%7d%0d%0a++++%7d%0d%0a++%5d%0d%0a%7d&pretty=true&from=20000&size=10000')
+
+//     //                     // .then(res => res.json());
+//     // let tmp = result.data.data.hits;
+//     // // console.log(tmp);
+//     // tmp.map((val) => data.push(val.id));
+//     console.log(data);
+// }
+
+// getdata();
+
+// export default getData; 
