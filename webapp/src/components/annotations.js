e58913aabc32d5c8c3f7f9268d8d9b5d8b82eadd
namandangi <danginaman55@gmail.com>
2021-04-23 22:53:52 +0530

    add annotation save tool

---
+++ webapp/src/components/annotations.js
@@ -0,0 +1,94 @@
+import React from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { FaSave } from "react-icons/fa";
+import {
+  useFabricOverlayDispatch,
+  useFabricOverlayState,
+} from "../context/fabric-overlay-context";
+import { useParams } from "react-router-dom";
+import AltButton from "./altButton";
+
+export default function MyAnnotationsSave() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const params = useParams;
+  const [title, setTitle] = React.useState("");
+  const {
+    activeUserCanvas,
+    fabricOverlay,
+    userCanvases,
+  } = useFabricOverlayState();
+  const dispatch = useFabricOverlayDispatch();
+
+  React.useEffect(() => {
+    setTitle(activeUserCanvas);
+  }, [activeUserCanvas]);
+
+  const handleSaveCanvas = () => {
+    let newCanvases = {
+      ...userCanvases,
+      [title]: {
+        locWorkId: params.id,
+        fabricCanvas: fabricOverlay._fabricCanvas.toObject(),
+      },
+    };
+    console.log(newCanvases);
+    dispatch({
+      type: "updateUserCanvases",
+      userCanvases: newCanvases,
+      activeUserCanvas: title,
+    });
+    onClose();
+  };
+
+  return (
+    <>
+      <AltButton disabled={false} onClick={onOpen} leftIcon={<FaSave />}>
+        Save
+      </AltButton>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Save annotation</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl
+              id="user-canvas-name"
+              isRequired
+              isInvalid={title === ""}
+            >
+              <FormLabel>Save as</FormLabel>
+            </FormControl>
+            <Input
+              placeholder="Name your work"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <FormErrorMessage>Value can't be empty</FormErrorMessage>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr={3} onClick={handleSaveCanvas} isDisabled={title === ""}>
+              Save
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+MyAnnotationsSave.propTypes = {};
