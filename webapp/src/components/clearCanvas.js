097eab542cb8ab6725b7935a0ad1ddfdc11304cf
namandangi <danginaman55@gmail.com>
2021-04-23 22:49:05 +0530

    add clear canvas tool

---
+++ webapp/src/components/clearCanvas.js
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+import AltButton from "./altButton";
+import useFabricHelpers from "../hooks/use-fabric-helpers";
+
+function ClearCanvas() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = React.useRef();
+  const { clearUserObjects } = useFabricHelpers();
+
+  const handleClearCanvas = () => {
+    clearUserObjects();
+    onClose();
+  };
+
+  return (
+    <>
+      <AltButton leftIcon={<DeleteIcon />} onClick={() => setIsOpen(true)}>
+        Clear
+      </AltButton>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Clear Canvas
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? This will remove all current annotations from the
+              canvas.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose} variant="ghost">
+                Cancel
+              </Button>
+              <Button onClick={handleClearCanvas} ml={3}>
+                Clear
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+}
+
+ClearCanvas.propTypes = {};
+
+export default ClearCanvas;
