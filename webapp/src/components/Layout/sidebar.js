b909ca60c3b6af93a5bffd54eb3060e2b85c17d1
namandangi <danginaman55@gmail.com>
2021-04-23 22:28:04 +0530

    add side toolbar

---
+++ webapp/src/components/Layout/sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+
+export default function LayoutAppSidebar({ children }) {
+  return (
+    <Flex
+      as="section"
+      w={{ base: "50px", sm: "60px", md: "80px" }}
+      direction="column"
+      justifyContent="space-between"
+      alignItems="center"
+      boxShadow="base"
+      zIndex="1"
+      bg="#2f3136"
+    >
+      {children}
+    </Flex>
+  );
+}
