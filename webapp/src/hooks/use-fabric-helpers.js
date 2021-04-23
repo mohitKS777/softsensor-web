41566ce4f0221a60aff963dc5107e6e750185f34
namandangi <danginaman55@gmail.com>
2021-04-23 21:48:04 +0530

    add helper hooks

---
+++ webapp/src/hooks/use-fabric-helpers.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useFabricOverlayState } from "../context/fabric-overlay-context";
+
+export default function useCanvasHelpers() {
+  const { activeTool, fabricOverlay } = useFabricOverlayState();
+  const [canvas, setCanvas] = React.useState();
+
+  React.useEffect(() => {
+    if (!fabricOverlay) return;
+    setCanvas(fabricOverlay.fabricCanvas());
+  }, [fabricOverlay]);
+
+  // Remove all Fabric canvas objects
+  const clearCanvas = () => {
+    if (!canvas) return;
+    canvas.clear();
+  };
+
+  const clearUserObjects = () => {
+    if (!canvas) return;
+    const userObjects = getUserObjects();
+    for (let i in userObjects) {
+      canvas.remove(userObjects[i]);
+    }
+  };
+
+  // Deselect all Fabric canvas objects
+  const deselectAll = () => {
+    if (!canvas) return;
+
+    canvas.discardActiveObject();
+    canvas.requestRenderAll();
+  };
+
+  const getUserObjects = () => {
+    if (!canvas) return;
+    const objects = canvas.getObjects();
+    const selectableObjects = objects.filter((obj) => obj.selectable);
+    return selectableObjects;
+  };
+
+  const makeObjectsInvisible = (fabricObjects = []) => {
+    if (!canvas) return;
+
+    for (let obj of fabricObjects) {
+      obj.opacity = 0;
+    }
+    canvas.renderAll();
+  };
+
+  const makeObjectsVisible = (fabricObjects = []) => {
+    for (let obj of fabricObjects) {
+      obj.opacity = 1;
+    }
+    canvas.renderAll();
+  };
+
+  const updateCursor = () => {
+    if (!canvas) return;
+
+    canvas.defaultCursor = "auto";
+    canvas.hoverCursor = "move";
+  };
+
+  return {
+    clearCanvas,
+    clearUserObjects,
+    deselectAll,
+    getUserObjects,
+    makeObjectsInvisible,
+    makeObjectsVisible,
+    updateCursor,
+  };
+}
---
+++ webapp/src/hooks/use-keyboard-events.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { useFabricOverlayState } from "../context/fabric-overlay-context";
+
+// https://stackoverflow.com/questions/44320104/fabricjs-how-to-move-the-selected-object-by-keyboard
+export default function useKeyboardEvents() {
+  const { fabricOverlay } = useFabricOverlayState();
+
+  function handleEvent(e) {
+    if (e.repeat) {
+      return;
+    }
+
+    var key = e.key;
+    const canvas = fabricOverlay.fabricCanvas();
+    const activeObject = canvas.getActiveObject();
+    //const activeGroup = canvas.getActiveGroup();
+    let activeGroup;
+    if (!activeObject) return;
+
+    const STEP = 10;
+
+    function moveSelected(direction) {
+      if (activeObject) {
+        switch (direction) {
+          case "LEFT":
+            activeObject.left = activeObject.left - STEP;
+            break;
+          case "UP":
+            activeObject.top = activeObject.top - STEP;
+            break;
+          case "RIGHT":
+            activeObject.left = activeObject.left + STEP;
+            break;
+          case "DOWN":
+            activeObject.top = activeObject.top + STEP;
+            break;
+        }
+        activeObject.setCoords();
+        canvas.renderAll();
+      }
+      // TODO: Wire this up if we actually need it?
+      else if (activeGroup) {
+        switch (direction) {
+          case direction.LEFT:
+            activeGroup.left = activeGroup.left - STEP;
+            break;
+          case direction.UP:
+            activeGroup.top = activeGroup.top - STEP;
+            break;
+          case direction.RIGHT:
+            activeGroup.left = activeGroup.left + STEP;
+            break;
+          case direction.DOWN:
+            activeGroup.top = activeGroup.top + STEP;
+            break;
+        }
+        activeGroup.setCoords();
+        canvas.renderAll();
+        console.log("selected group was moved");
+      } else {
+        console.log("no object selected");
+      }
+    }
+
+    if (key === "ArrowLeft") {
+      // handle Left key
+      moveSelected("LEFT");
+    } else if (key === "ArrowUp") {
+      // handle Up key
+      moveSelected("UP");
+    } else if (key === "ArrowRight") {
+      // handle Right key
+      moveSelected("RIGHT");
+    } else if (key === "ArrowDown") {
+      // handle Down key
+      moveSelected("DOWN");
+    } else if (key === "Backspace" || key === "Clear") {
+      // Handle Delete key
+      // Object has children (ie. arrow has children objects triangle and line)
+      if (activeObject.getObjects) {
+        let objs = activeObject.getObjects();
+        for (let i in objs) {
+          canvas.remove(objs[i]);
+        }
+      }
+      canvas.remove(activeObject);
+    }
+  }
+
+  return { handleEvent };
+}
