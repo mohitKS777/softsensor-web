89b8289c59c36a57ae98967e53bfd52c1c1141e0
namandangi <danginaman55@gmail.com>
2021-04-23 22:32:39 +0530

    add wrapper for OSD Fabricjs

---
+++ webapp/src/components/osdFabricWrapper.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ViewerProvider } from "use-open-seadragon";
+import { FabricOverlayProvider } from "../context/fabric-overlay-context";
+
+export default function OsdFabricWrapper({ children }) {
+  return (
+    <ViewerProvider>
+      <FabricOverlayProvider>{children}</FabricOverlayProvider>
+    </ViewerProvider>
+  );
+}
