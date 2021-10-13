2bae7f299fa891ca688d48f9f02313900dd45c76
Naman <45919359+namandangi@users.noreply.github.com>
2021-10-13 18:55:17 +0530

    Revert "Dashboard and Login Basics (#39)" (#40)
    This reverts commit f914bab87fa3d4028ff8860c3d01bd746b13bedb.

--- webapp/src/components/Authenticate/login.js
+++ webapp/src/components/Authenticate/login.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { Button } from "@chakra-ui/react"
 import { useAuth0 } from "@auth0/auth0-react";
+import AltButton from "../altButton";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return (
-    <Button
-      backgroundColor="#3965C6"
-      color="white"
-      marginBottom="1em"
-      _hover={{ bg: "#66a3ff" }}
-      onClick={() => loginWithRedirect()}>
-      Sign In
-    </Button>
-  );
+  return <AltButton onClick={() => loginWithRedirect()}>Log In</AltButton>;
 };
 
 export default LoginButton;
