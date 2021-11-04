463674d0ec3b3147e76e7c08558031284bc32bdb
mohitks77 <mohitks3000@gmail.com>
2021-11-04 12:57:04 +0530

    create new project screens

--- webapp/src/components/Dashboard/dashboard.js
+++ webapp/src/components/Dashboard/dashboard.js
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
-import {
-    Flex,
-    Spacer,
-    Text,
-    useMediaQuery
-} from "@chakra-ui/react";
+import { Flex, Spacer, Text, useMediaQuery } from "@chakra-ui/react";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
 import DashboardMenu from "./menu";
 import Gallery from "./gallery";
@@ -16,49 +11,63 @@ import Recent from "./recent";
 import NewAssigned from "./newAssigned";
 import LastTask from "./lastTask";
 import LastReports from "./lastReports";
+import Newproject from "../Newproject/newproject";
+import Newslides from "../Newproject/selectslides";
+import Questionnaire from "../Newproject/questionnaire";
 
 const Dashboard = ({ subClaim }) => {
-    const { user } = useAuth0();
-    const data = { subClaim: subClaim }
-    // const resp = axios.post('http://localhost:3001/api/get_user_information', data, {headers : `authorization" : "bearer ${token}`})
-    // const [id, setId] = useState(user?.nickname);
-    // const history = useHistory();
-    // const handleProceed = (e) => {
-    //     id && history.push(generatePath("/dashboard/:id", { id }));
-    // };
-    // handleProceed();
+  const { user } = useAuth0();
+  const data = { subClaim: subClaim };
+  // const resp = axios.post('http://localhost:3001/api/get_user_information', data, {headers : `authorization" : "bearer ${token}`})
+  // const [id, setId] = useState(user?.nickname);
+  // const history = useHistory();
+  // const handleProceed = (e) => {
+  //     id && history.push(generatePath("/dashboard/:id", { id }));
+  // };
+  // handleProceed();
 
-    const breakpoints = createBreakpoints({
-        sm: "1280px",
-        md: "1440px",
-        lg: "1920px",
-        xl: "2560px",
-    });
+  const breakpoints = createBreakpoints({
+    sm: "1280px",
+    md: "1440px",
+    lg: "1920px",
+    xl: "2560px",
+  });
 
-    return (
-        <>
-            <DashboardMenu />
-            <Flex marginLeft="14em" height="100vh" direction="column" backgroundColor="#eeeeee">
-                <Flex w="250" bg="#3965C5" color="white" py={5} px={10} zIndex="1" sx={{ position: '-webkit-sticky', position: 'sticky', top: '0', }}>
-                    <Text fontSize="2em">
-                        Welcome
-                    </Text>
-                    <Spacer />
-                    <Search w={300}/>
-                </Flex>
-                <Flex height="100%" w="100%" direction="row" marginTop="20px">
-                    <Flex height="100%" w="100%" direction="column">
-                        <Recent />
-                        <NewAssigned />
-                    </Flex>
-                    <Flex height="100%" w="30%" direction="column" marginRight="20px">
-                        <LastTask />
-                        <LastReports />
-                    </Flex>
-                </Flex>
-            </Flex>
-        </>
-    );
+  return (
+    <>
+      <DashboardMenu />
+      <Flex
+        marginLeft="14em"
+        height="100vh"
+        direction="column"
+        backgroundColor="#eeeeee"
+      >
+        <Flex
+          w="250"
+          bg="#3965C5"
+          color="white"
+          py={5}
+          px={10}
+          zIndex="1"
+          sx={{ position: "-webkit-sticky", position: "sticky", top: "0" }}
+        >
+          <Text fontSize="2em">Welcome</Text>
+          <Spacer />
+          <Search w={300} />
+        </Flex>
+        <Flex height="100%" w="100%" direction="row" marginTop="20px">
+          <Flex height="100%" w="100%" direction="column">
+            <Recent />
+            <NewAssigned />
+          </Flex>
+          <Flex height="100%" w="30%" direction="column" marginRight="20px">
+            <LastTask />
+            <LastReports />
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
 };
 
 export default Dashboard;
--- webapp/src/components/Dashboard/menu.js
+++ webapp/src/components/Dashboard/menu.js
@@ -1,114 +1,132 @@
 import React, { useState, useEffect } from "react";
+import { Link as RouteLink } from "react-router-dom";
 import {
-    Box,
-    Button,
-    Center,
-    Flex,
-    Icon,
-    Image,
-    Link,
-    Menu,
-    MenuButton,
-    MenuDivider,
-    MenuGroup,
-    MenuItem,
-    MenuList,
-    Text
+  Box,
+  Button,
+  Center,
+  Flex,
+  Icon,
+  Image,
+  Link,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuGroup,
+  MenuItem,
+  MenuList,
+  Text,
 } from "@chakra-ui/react";
 import { BiTime } from "react-icons/bi";
 import { BsCircleFill } from "react-icons/bs";
 import { IoAdd } from "react-icons/io5";
-import { AiOutlineFile, AiOutlineFolderOpen, AiOutlineProject } from "react-icons/ai";
+import {
+  AiOutlineFile,
+  AiOutlineFolderOpen,
+  AiOutlineProject,
+} from "react-icons/ai";
 import { useAuth0 } from "@auth0/auth0-react";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
 import LogoutButton from "../Authenticate/logout";
+import Newproject from "../Newproject/newproject";
 
 const DashboardMenu = () => {
-    const { user, isAuthenticated } = useAuth0();
-    const [emailId, setEmailId] = useState();
-    const [userImage, setUserImage] = useState();
+  const { user, isAuthenticated } = useAuth0();
+  const [emailId, setEmailId] = useState();
+  const [userImage, setUserImage] = useState();
+  const [newProjectScreen, setNewProjectScreen] = useState(false);
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            setEmailId(user?.email);
-            setUserImage(user?.picture);
-        }
-    }, [isAuthenticated]);
+  useEffect(() => {
+    if (isAuthenticated) {
+      setEmailId(user?.email);
+      setUserImage(user?.picture);
+    }
+  }, [isAuthenticated]);
 
-    const breakpoints = createBreakpoints({
-        sm: "1280px",
-        md: "1440px",
-        lg: "1920px",
-        xl: "2560px",
-    });
+  const breakpoints = createBreakpoints({
+    sm: "1280px",
+    md: "1440px",
+    lg: "1920px",
+    xl: "2560px",
+  });
 
-    return (
-        <Menu defaultIsOpen={true} closeOnBlur={false} closeOnSelect={false} autoSelect={false}>
-            <MenuList
-                bg="#3965C6"
-                height="100vh"
-                width={{base: "100%", sm: "50%", md: "25%"}}
-                borderRadius="0px"
-                border="none"
-                color="white"
-                sx={{ position: '-webkit-sticky', position: 'sticky', bottom: '0', top: '0'}}>
-                <Flex>
-                    <Image
-                        borderRadius="full"
-                        boxSize="50px"
-                        mx={3}
-                        marginTop="1em"
-                        src={userImage}
-                        alt="User"
-                    />
-                    <Flex marginTop="1em" direction="column" width="9em">
-                        <Text color="white" >User's Name</Text>
-                        <Text fontSize="xs">{emailId}</Text>
-                        <Link fontSize="small" color="white" marginTop="10px">
-                            Manage your Account
-                        </Link>
-                    </Flex>
-                </Flex>
-                <MenuGroup>
-                    <MenuDivider marginTop="1em" mx={2} />
-                    <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
-                        <Icon as={BiTime} marginRight={2} w={5} h={7} />
-                        Recently Viewed
-                    </MenuItem>
-                    <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
-                        <Icon as={AiOutlineProject} marginRight={2} w={5} h={7} />
-                        Projects
-                    </MenuItem>
-                    <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
-                        <Icon as={AiOutlineFile} marginRight={2} w={5} h={7} />
-                        New Projects
-                    </MenuItem>
-                    <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
-                        <Icon as={AiOutlineFolderOpen} marginRight={2} w={5} h={7} />
-                        Open
-                    </MenuItem>
-                    <MenuDivider m={2} />
-                    <MenuItem _hover={{ bg: "#66a3ff" }} fontWeight="bold">
-                        <Icon as={BsCircleFill} marginRight={2} w={4} h={4} />
-                        User's Name
-                    </MenuItem>
-                    <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
-                        Team Project
-                    </MenuItem>
-                    <MenuDivider m={2} />
-                    <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
-                        <Icon as={IoAdd} marginRight={2} w={5} h={7} />
-                        Create New Team
-                    </MenuItem>
-                    <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
-                        <Icon as={BsCircleFill} marginRight={2} w={4} h={4} />
-                        Help
-                    </MenuItem>
-                    <LogoutButton />
-                </MenuGroup>
-            </MenuList>
-        </Menu>
-    );
+  return (
+    <Menu
+      defaultIsOpen={true}
+      closeOnBlur={false}
+      closeOnSelect={false}
+      autoSelect={false}
+    >
+      <MenuList
+        bg="#3965C6"
+        height="100vh"
+        width={{ base: "100%", sm: "50%", md: "25%" }}
+        borderRadius="0px"
+        border="none"
+        color="white"
+        sx={{
+          position: "-webkit-sticky",
+          position: "sticky",
+          bottom: "0",
+          top: "0",
+        }}
+      >
+        <Flex>
+          <Image
+            borderRadius="full"
+            boxSize="50px"
+            mx={3}
+            marginTop="1em"
+            src={userImage}
+            alt="User"
+          />
+          <Flex marginTop="1em" direction="column" width="9em">
+            <Text color="white">User's Name</Text>
+            <Text fontSize="xs">{emailId}</Text>
+            <Link fontSize="small" color="white" marginTop="10px">
+              Manage your Account
+            </Link>
+          </Flex>
+        </Flex>
+        <MenuGroup>
+          <MenuDivider marginTop="1em" mx={2} />
+          <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
+            <Icon as={BiTime} marginRight={2} w={5} h={7} />
+            Recently Viewed
+          </MenuItem>
+          <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
+            <Icon as={AiOutlineProject} marginRight={2} w={5} h={7} />
+            Projects
+          </MenuItem>
+          <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
+            <Icon as={AiOutlineFile} marginRight={2} w={5} h={7} />
+            New Projects
+          </MenuItem>
+          <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
+            <Icon as={AiOutlineFolderOpen} marginRight={2} w={5} h={7} />
+            Open
+          </MenuItem>
+          <MenuDivider m={2} />
+          <MenuItem _hover={{ bg: "#66a3ff" }} fontWeight="bold">
+            <Icon as={BsCircleFill} marginRight={2} w={4} h={4} />
+            User's Name
+          </MenuItem>
+          <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
+            Team Project
+          </MenuItem>
+          <MenuDivider m={2} />
+          <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
+            <Icon as={IoAdd} marginRight={2} w={5} h={7} />
+            Create New Team
+          </MenuItem>
+          <MenuItem _hover={{ bg: "#66a3ff" }} fontSize="small">
+            <Icon as={BsCircleFill} marginRight={2} w={4} h={4} />
+            Help
+          </MenuItem>
+          <LogoutButton />
+        </MenuGroup>
+      </MenuList>
+    </Menu>
+  );
 };
 
 export default DashboardMenu;
