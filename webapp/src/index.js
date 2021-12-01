337d06678ed10446d6f11aa218fcd252f16543ca
namandangi <danginaman55@gmail.com>
2021-12-01 13:36:35 +0530

    fix audience auth0

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     redirectUri={process.env.REACT_APP_AUTH0_REDIRECT_URI}
-//     audience="http://localhost:3001"
+    audience={process.env.REACT_APP_AUTH0_AUDIENCE}
     // scope="read:users"
   >
     <Provider store={store}>
