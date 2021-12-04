075ab2e6e9f9f202cb7c1373b6aa1da46abf5594
mhatheshssai <89118002+mhatheshssai@users.noreply.github.com>
2021-12-04 20:44:24 +0530

    reverted the changes doe in app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 const path = require("path");
 const { spawn } = require("child_process");
 
-//app.use(cors());
-app.options('*', cors()) 
+app.use(cors());
+//app.options('*', cors()) 
 
 let guestList = [];
 
